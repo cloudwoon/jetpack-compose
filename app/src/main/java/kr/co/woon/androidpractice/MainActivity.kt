c9053a4bfd11262625df5743167819da5df0c9a1
cloudwoon <cloud.wooo0n@gmail.com>
2023-04-27 22:08:33 +0900

    compose modifier 사용해보기

--- app/src/main/java/kr/co/woon/androidpractice/MainActivity.kt
+++ app/src/main/java/kr/co/woon/androidpractice/MainActivity.kt
@@ -1,66 +1,124 @@
 package kr.co.woon.androidpractice
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import kr.co.woon.androidpractice.ui.theme.AndroidPracticeTheme
 
 
-//컴포즈는 선언형
-/*명령형과 선언형이 있음.
-선언형 : 결과만 명시 -> 알고리즘이 필요 없음*//*
-명령형 : 과정을 명시 -> 알고리즘을 작성해야 함*/
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val field = remember { mutableStateOf("") }
-            val fields = remember { mutableStateListOf<String>() }
-            Column(
-                modifier = Modifier.padding(30.dp),
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    var selectedItem by remember { mutableStateOf(0) }
+                    val items = remember {
+                        listOf("Songs", "Artists", "Playlists")
+                    }
+
+                    BottomNavigation {
+                        items.forEachIndexed { index, item ->
+                            BottomNavigationItem(
+                                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                                label = { Text(item) },
+                                selected = selectedItem == index,
+                                onClick = { selectedItem = index }
+                            )
+                        }
+                    }
+                }
             ) {
-                Row {
-                    TextField(
-                        value = field.value,
-                        onValueChange = { field.value = it },
-                    )
-                    Button(onClick = {
-                        fields.add(field.value)
-                        field.value = ""
-                    }) {
-                        Text(text = "TODO")
+                val field = remember { mutableStateOf("") }
+                val fields = remember { mutableStateListOf<String>() }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                        .padding(30.dp),
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        TextField(
+                            modifier = Modifier.width(250.dp),
+                            value = field.value,
+                            onValueChange = { field.value = it },
+                            singleLine = true,
+                            maxLines = 1,
+                            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
+                            keyboardActions = KeyboardActions {
+                                Toast.makeText(this@MainActivity, "SENT", Toast.LENGTH_SHORT).show()
+                            },
+                        )
+                        Button(
+                            onClick = {
+                                fields.add(field.value)
+                                field.value = ""
+                            },
+                        ) {
+                            Text(text = "TODO")
+                        }
+                    }
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(15.dp),
+                    ) {
+                        items(fields) { item ->
+                            Card(modifier = Modifier.fillMaxWidth()) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 8.dp),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                ) {
+                                    val check = remember { mutableStateOf(false) }
+                                    Text(text = item)
+                                    Checkbox(checked = check.value, onCheckedChange = { check.value = !check.value })
+                                }
+                            }
+                        }
                     }
                 }
-                fields.forEach{
-                    Row {
-                        val check = remember { mutableStateOf(false) }
-                        Text(text = it)
-                        Checkbox(checked = check.value, onCheckedChange = {check.value = !check.value})
-
-                    }
-
-                }
-
             }
-
         }
     }
 }
-
-
-//composition
-//recomposition - UI 변화가 생겼을 때 다시 유아이를 그려주는 것
-//리컴포지션을 시키기 위해서는 state라는 것을 사용함.
-//그래서 state 관리를 잘 해줘야 함.
-
